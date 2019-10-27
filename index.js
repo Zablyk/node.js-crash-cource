ee749cd35cbd1685c71767e6fa84feb39e03873d
Oleg Zablotskyi <zablotsky@gmail.com>
2019-10-27 13:27:19 -0400

    installed nodemon and added JSON.parse

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 const fs = require('fs');
-const http = require('http');
 
 const json = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
 const laptopData = JSON.parse(json);
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-type': 'text/html'});
-    res.end('This is rsponce!');
-});
-server.listen(1337, '127.0.0.1', () => {
-    console.log('Listening for a request now!')
-});
-
+console.log(laptopData);
