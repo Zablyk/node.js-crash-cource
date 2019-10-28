1320ee214b3846ba079075ea79b7ee7d8923a634
Oleg Zablotskyi <Zablotsky@gmail.com>
2019-10-28 16:45:33 -0400

    have added a url module

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
 const json = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
 const laptopData = JSON.parse(json);
 
 const server = http.createServer((req, res) => {
-    console.log('Someone did acces a server!');
+
+    const pathName = url.parse(req.url, true).pathname;
+    
+    if (pathName === 'products') {
+        res.writeHead(200, { 'Content-type': 'text/html'});
+        res.end('This is a response!');
+    }
+    
 });
 
 server.listen(1337, '127.0.0.1', () => {
     console.log('Listening for a request now');
 });
 
-console.log('lets try it');
