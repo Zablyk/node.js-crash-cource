f96bbeca797a27e4345c7108f5936a8a9317b21f
Oleg Zablotskyi <zablotsky@gmail.com>
2019-10-28 22:48:56 -0400

    now we can load a PRODUCTS page

--- index.js
+++ index.js
@@ -9,12 +9,25 @@ const server = http.createServer((req, res) => {
 
     const pathName = url.parse(req.url, true).pathname;
     const id = url.parse(req.url, true).query.id;
-    
+
+    //PRODUCT OWERVIEW
     if (pathName === '/products' || pathName === '/') {
         res.writeHead(200, { 'Content-type': 'text/html'});
         
+        fs.readFile(`${__dirname}/templates/template-overview.html`, 'utf-8', (err, data) => {
+            let overviewOutput = data;
+            
+            fs.readFile(`${__dirname}/templates/template-card.html`, 'utf-8', (err, data) => {
+                const cardsOutput = laptopData.map(el => replaceTemplate(data, el)).join('');
+                overviewOutput = overviewOutput.replace( '{%CARDS%}', cardsOutput);
+                res.end(overviewOutput);
+            });
+        });
+
+    
     }
 
+    //LAPTOP DETAIL
     else if (pathName === '/laptop' && id < laptopData.length) {
         res.writeHead(200, { 'Content-type': 'text/html'});
 
@@ -25,6 +38,7 @@ const server = http.createServer((req, res) => {
         });
     }
 
+    //URL NOT FOUND
     else {
         res.writeHead(404, { 'Content-type': 'text/html'});
         res.end('URL was not found on a website!');
