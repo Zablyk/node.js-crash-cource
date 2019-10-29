81fdd45f1d2465f25c939993063190b6be89b342
Oleg Zablotskyi <zablotsky@gmail.com>
2019-10-28 22:19:19 -0400

    have aaded replaceTamplate function

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const server = http.createServer((req, res) => {
     
     if (pathName === '/products' || pathName === '/') {
         res.writeHead(200, { 'Content-type': 'text/html'});
-        res.end('This is a PRODUCT page');
+        
     }
 
     else if (pathName === '/laptop' && id < laptopData.length) {
@@ -20,17 +20,9 @@ const server = http.createServer((req, res) => {
 
         fs.readFile(`${__dirname}/templates/template-laptop.html`, 'utf-8', (err, data) => {
             const laptop = laptopData[id];
-            let output = data.replace(/{%PRODUCTNAME%}/g, laptop.productName);
-            output = output.replace(/{%IMAGE%}/g, laptop.image);
-            output = output.replace(/{%PRICE%}/g, laptop.price);
-            output = output.replace(/{%SCREEN%}/g, laptop.screen);
-            output = output.replace(/{%CPU%}/g, laptop.cpu);
-            output = output.replace(/{%STORAGE%}/g, laptop.storage);
-            output = output.replace(/{%RAM%}/g, laptop.ram);
-            output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
+            const output = replaceTemplate(data, laptop)
             res.end(output);
         });
-        //res.end(`This is a LAPTOP page for laptop ${id}`);
     }
 
     else {
@@ -44,3 +36,15 @@ server.listen(1337, '127.0.0.1', () => {
     console.log('Listening for a request now');
 });
 
+function replaceTemplate(originalHtml, laptop) {
+    let output = originalHtml.replace(/{%PRODUCTNAME%}/g, laptop.productName);
+    output = output.replace(/{%IMAGE%}/g, laptop.image);
+    output = output.replace(/{%PRICE%}/g, laptop.price);
+    output = output.replace(/{%SCREEN%}/g, laptop.screen);
+    output = output.replace(/{%CPU%}/g, laptop.cpu);
+    output = output.replace(/{%STORAGE%}/g, laptop.storage);
+    output = output.replace(/{%RAM%}/g, laptop.ram);
+    output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
+    output = output.replace(/{%ID%}/g, laptop.id);
+    return output;
+};
